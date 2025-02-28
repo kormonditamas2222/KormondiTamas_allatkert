e9a9e14463b5589b85811d38d90680333d80f669
kormonditamas2222 <t.kormond2006@gmail.com>
2025-02-28 12:26:16 +0100

    main alapok

--- allatkert/Program.cs
+++ allatkert/Program.cs
@@ -4,7 +4,16 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
-		}
+			ViziAllat vizi = new("Delfin", "nőstény", 14, 3);
+			SzarazfoldiHideg hideg = new("Pingvin", "hím", 6, 1);
+			SzarazfoldiMediterran mediterran = new("Kanári", "tojó", 1, 2);
+			SzarazfoldiMeleg meleg = new("Gepárd", "hím", 20, 5);
+			SzarazfoldiTropusi tropusi = new("Gyűrűsfarkú maki", "hím", 9, 3);
+			Allatkert allatkert = new();
+			List<String> nevek = allatkert.AllatokNeve(vizi, hideg, mediterran, meleg, tropusi);
+			List<int> sebessegek = allatkert.AllatokSebessege(vizi, hideg, mediterran, meleg, tropusi);
+
+			
+        }
 	}
 }
