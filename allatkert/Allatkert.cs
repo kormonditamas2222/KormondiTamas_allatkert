10127e4100ddbfa7e6254a5f9df68fef0cec8ddd
kormonditamas2222 <t.kormond2006@gmail.com>
2025-02-28 12:15:48 +0100

    classok kész

---
+++ allatkert/Allatkert.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace allatkert
+{
+	internal class Allatkert
+	{
+		static Random random = new();
+
+		private int meret;
+
+		public Allatkert()
+		{
+			this.meret = random.Next(5, 16);
+		}
+
+		public int Meret { get => meret; set => meret = value; }
+		
+		public List<String> AllatokNeve(ViziAllat vizi, SzarazfoldiHideg hideg, SzarazfoldiMediterran mediterran, SzarazfoldiMeleg meleg, SzarazfoldiTropusi tropusi)
+		{
+			List<String> nevek = new();
+			nevek.Add(vizi.Nev);
+			nevek.Add(hideg.Nev);
+			nevek.Add(mediterran.Nev);
+			nevek.Add(meleg.Nev);
+			nevek.Add(tropusi.Nev);
+			return nevek;
+		}
+
+		public List<int> AllatokSebessege(ViziAllat vizi, SzarazfoldiHideg hideg, SzarazfoldiMediterran mediterran, SzarazfoldiMeleg meleg, SzarazfoldiTropusi tropusi)
+		{
+			List<int> sebessegek = new();
+			sebessegek.Add(vizi.Sebesseg);
+			sebessegek.Add(hideg.Sebesseg);
+			sebessegek.Add(mediterran.Sebesseg);
+			sebessegek.Add(meleg.Sebesseg);
+			sebessegek.Add(tropusi.Sebesseg);
+			return sebessegek;
+		}
+	}
+}
---
+++ allatkert/SzarazfoldiHideg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace allatkert
+{
+	internal class SzarazfoldiHideg
+	{
+		private string nev;
+		private string nem;
+		private int tomeg;
+		private int sebesseg;
+
+		public SzarazfoldiHideg(string nev, string nem, int tomeg, int sebesseg)
+		{
+			this.nev = nev;
+			this.nem = nem;
+			this.tomeg = tomeg;
+			this.sebesseg = sebesseg;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public string Nem { get => nem; set => nem = value; }
+		public int Tomeg { get => tomeg; set => tomeg = value; }
+		public int Sebesseg { get => sebesseg; set => sebesseg = value; }
+	}
+}
